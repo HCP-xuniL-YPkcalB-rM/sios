0a18d348087e8e2e7b2cece85f12a6e73115f030
HCP-xuniL-YPkcalB-rM <42275675+HCP-xuniL-YPkcalB-rM@users.noreply.github.com>
2018-08-16 10:29:24 +0430

    Update sios.py

--- sios.py
+++ sios.py
@@ -1,27 +1,78 @@
-/usr/bin/python3.6
+#!/usr/bin/python3.6
 _author_ = "Mr.BlackPY"
 #Showing Information Of System [sios Project]
+#GNU/LINUX
 
 import sys,os,datetime
+
+version = "0.2"
+
+print("-----------------------------")
 
 print("Platform name:")
 print(sys.platform)
 
+print("-----------------------------")
+
 print("os name:")
 print(os.name)
+
+print("-----------------------------")
 
 print("os Path:")
 print(os.getenv('PATH'))
 
+print("-----------------------------")
+
+print("Shell:")
+print(os.getenv('SHELL'))
+
+print("-----------------------------")
+
+print("Language and encoding:")
+print(os.getenv('LC_NAME'))
+
+print("-----------------------------")
+
+print("SSH agent pid:")
+print(os.getenv('SSH_AGENT_PID'))
+
+print("-----------------------------")
+
 print("current Path:")
 print(os.getcwd())
+
+print("-----------------------------")
 
 print("current date & time:")
 now = datetime.datetime.now()
 print(now)
 
+print("-----------------------------")
+
 print("current user:")
 print(os.getlogin())
 
-print("default encoding:")
-print(sys.getdefaultencoding())
+print("-----------------------------")
+
+print("Session manager:")
+print(os.getenv('SESSION_MANAGER'))
+
+print("-----------------------------")
+
+print("Term:")
+print(os.getenv('TERM'))
+
+print("-----------------------------")
+
+print("Text domain dir:")
+print(os.getenv('TEXTDOMAINDIR'))
+
+print("-----------------------------")
+
+print("Home:")
+print(os.getenv('HOME'))
+
+print("-----------------------------")
+
+#Creator: Mr.BlackPY
